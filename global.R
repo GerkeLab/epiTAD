0244b9cfdeab411554e0f9a0104ae870379f6f59
Garrick Aden-Buie <Garrick.Aden-Buie@moffitt.org>
2019-04-09 13:59:36 -0400

    Treat chrX/Y in TAD/LAD as missing

--- global.R
+++ global.R
@@ -19,12 +19,14 @@ load("data/hicData.Rdata")
 
 tad <- fread("data/IMR90_domains_hg19.bed")
 colnames(tad) <- c("chr", "start_position", "end_position")
+tad$chr[tad$chr == "chrX"] <- NA_character_
 tad$chr <- gsub("chr", "", tad$chr)
 tad$chr <- as.numeric(tad$chr)
 tad <- tad[!is.na(tad$chr), ]
 
 lad <- fread("data/human.fibroblast.DamID.hg19.bed")
 colnames(lad) <- c("chr", "start", "end", "dunno")
+lad$chr[lad$chr %in% c("chrX", "chrY")] <- NA_character_
 lad$chr <- gsub("chr", "", lad$chr)
 lad$chr <- as.numeric(lad$chr)
 lad <- lad[!is.na(lad$chr), ]
